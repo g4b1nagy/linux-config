86f2bb2938d2f17242a5e6f00319e037caef3773
Gabi Nagy <gabrian.nagy@gmail.com>
2023-08-31 12:56:24 +0200

    Update debian-12-bookworm-xfce.sh

--- debian-12-bookworm-xfce.sh
+++ debian-12-bookworm-xfce.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
 
 # =========================================================================
 # Prerequisites
 # =========================================================================
 
-username="john"
+# Add yourself to the sudo group.
+su --login
+adduser john sudo
+
+# Log out and log back in.
+
 full_name="John Doe"
 email="john@doe.com"
-
-# Add user to sudo group
-usermod -aG sudo $username
 
 # =========================================================================
 # Install packages
@@ -30,7 +35,7 @@ packages=(
     nginx
     postgresql
     redis-server
-    redis-tools
+    sublime-text
     viewnior
     vim
     vlc
@@ -40,15 +45,18 @@ packages=(
     mtp-tools
 )
 
-sudo apt install "${packages[@]}"
+# Add Sublime Text apt repository.
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
-# + https://www.sublimetext.com/docs/linux_repositories.html
+sudo apt update
+sudo apt install "${packages[@]}"
 
 # =========================================================================
 # Configure Bash
 # =========================================================================
 
-cp /home/$USER/.bashrc /home/$USER/.bashrc_backup_$(date +%F_%T)
+cp ~/.bashrc ~/.bashrc_backup_$(date +%F_%T)
 
 echo "
 
@@ -58,25 +66,24 @@ echo "
 
 function cd {
     builtin cd \"\$@\"
-    if [ -d venv ]
+    if [ -d .venv ]
     then
-        source venv/bin/activate
+        source .venv/bin/activate
     fi
 }
 
-alias ls='ls -lh --color=auto'
 alias grep='grep --color=auto'
+alias ls='ls -l --human-readable --color=auto'
 alias py='python3'
 
-alias s='git status'
+alias c='git checkout'
 alias d='git diff --minimal'
 alias dc='git diff --minimal --cached'
-alias c='git checkout'
+alias s='git status'
 
 alias doff='xrandr --output eDP-1 --off'
 alias don='xrandr --output eDP-1 --auto'
-" >> /home/$USER/.bashrc
-
+" >> ~/.bashrc
 
 # =========================================================================
 # Configure Guake
@@ -134,31 +141,28 @@ palette='#1C1C1D1D1919:#D0D01B1B2424:#A7A7D3D32C2C:#D8D8CFCF6767:#6161B8B8D0D0:#
 palette-name='Monokai'
 " | dconf load /apps/guake/
 
-# For some reason, the ~/.config/autostart/ directory gets cleared out on reboot.
-# mkdir -p /home/$USER/.config/autostart/
-# cp /usr/share/applications/guake.desktop /home/$USER/.config/autostart/
 sudo cp /usr/share/applications/guake.desktop /etc/xdg/autostart/
 
 # =========================================================================
 # Configure Sublime Text
 # =========================================================================
 
-mkdir -p /home/$USER/.config/sublime-text/Packages/User/
+mkdir -p ~/.config/sublime-text/Packages/User/
 
 echo '[
     {"command": "expand_selection", "args": {"to": "line"}},
     {"command": "left_delete"}
-]' > /home/$USER/.config/sublime-text/Packages/User/DeleteLine.sublime-macro
+]' > ~/.config/sublime-text/Packages/User/DeleteLine.sublime-macro
 
-# Preferences => Key Bindings - User
+# Preferences => Key Bindings
 echo '[
     { "keys": ["ctrl+d"], "command": "run_macro_file", "args": {"file": "Packages/User/DeleteLine.sublime-macro"}},
     { "keys": ["ctrl+tab"], "command": "next_view" },
     { "keys": ["ctrl+shift+tab"], "command": "prev_view" },
     { "keys": ["ctrl+f"], "command": "show_panel", "args": { "panel": "find", "in_selection": false } }
-]' > /home/$USER/.config/sublime-text/Packages/User/Default\ \(Linux\).sublime-keymap
+]' > ~/.config/sublime-text/Packages/User/Default\ \(Linux\).sublime-keymap
 
-# Preferences => Settings - User
+# Preferences => Settings
 echo '{
     "auto_close_tags": false,
     "auto_complete": false,
@@ -166,7 +170,7 @@ echo '{
     "color_scheme": "Monokai.sublime-color-scheme",
     "enable_tab_scrolling": false,
     "ensure_newline_at_eof_on_save": true,
-    "folder_exclude_patterns": [".git", "venv", "__pycache__", "node_modules"],
+    "folder_exclude_patterns": [".git", ".venv", "__pycache__", "node_modules"],
     "font_size": 10,
     "mini_diff": false,
     "show_definitions": false,
@@ -178,51 +182,46 @@ echo '{
     "translate_tabs_to_spaces": true,
     "trim_only_modified_white_space": false,
     "trim_trailing_white_space_on_save": "all",
-}' > /home/$USER/.config/sublime-text/Packages/User/Preferences.sublime-settings
+}' > ~/.config/sublime-text/Packages/User/Preferences.sublime-settings
 
-# Add .ini file syntax highlighting
+# Add .ini syntax highlighting
 wget https://github.com/jwortmann/ini-syntax/archive/refs/tags/v1.5.2.tar.gz
-tar -xzvf v1.5.2.tar.gz --directory /home/$USER/.config/sublime-text/Packages/
+tar -xzvf v1.5.2.tar.gz --directory ~/.config/sublime-text/Packages/
 
-# Add .toml file syntax highlighting
+# Add .toml syntax highlighting
 wget https://github.com/jasonwilliams/sublime_toml_highlighting/archive/refs/tags/v2.5.0.tar.gz
-tar -xzvf v2.5.0.tar.gz --directory /home/$USER/.config/sublime-text/Packages/
+tar -xzvf v2.5.0.tar.gz --directory ~/.config/sublime-text/Packages/
 
-
-# =========================================================================
-# Configure fonts
-# =========================================================================
-
-# Remove Noto fonts so the system can default to DejaVu instead.
-
-# cp /usr/share/fontconfig/conf.avail/60-latin.conf ~/Documents/
-
-sudo cp /usr/share/fontconfig/conf.avail/60-latin.conf /usr/share/fontconfig/conf.avail/60-latin.conf_backup_$(date +%F_%T)
-sudo sed -i 's/.*Noto.*//g' /usr/share/fontconfig/conf.avail/60-latin.conf
-
+# Add .tf syntax highlighting
+wget https://github.com/alexlouden/Terraform.tmLanguage/archive/refs/tags/1.2.0.tar.gz
+tar -xzvf 1.2.0.tar.gz --directory ~/.config/sublime-text/Packages/
 
 # =========================================================================
 # Configure Vim
 # =========================================================================
 
-mkdir -p /home/$USER/.vim/colors/
-wget -P /home/$USER/.vim/colors/ https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+mkdir -p ~/.vim/colors/
+wget -P ~/.vim/colors/ https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
 
 echo "colorscheme molokai
 set expandtab
 set mouse-=a
 set shiftwidth=4
 set tabstop=4
-syntax on" > /home/$USER/.vimrc
+syntax on" > ~/.vimrc
 
-sudo update-alternatives --config editor
+update-alternatives --set editor /usr/bin/vim.basic
 
 # =========================================================================
-# Configure sqlite3
+# Configure Git
 # =========================================================================
 
-echo ".header on
-.mode table" > /home/$USER/.sqliterc
+git config --global advice.skippedCherryPicks false
+git config --global color.ui true
+git config --global init.defaultBranch master
+git config --global pull.rebase true
+git config --global user.email "${email}"
+git config --global user.name "${full_name}"
 
 # =========================================================================
 # Configure nginx
@@ -237,28 +236,45 @@ sudo sed -i '/^\tlocation \/ {/s/{/{\n\t\tautoindex on;/' /etc/nginx/sites-avail
 sudo systemctl restart nginx
 
 # =========================================================================
-# Configure Git
+# Configure sqlite3
 # =========================================================================
 
-git config --global user.name "$full_name"
-git config --global user.email "$email"
-git config --global pull.rebase true
-git config --global color.ui true
-git config --global init.defaultBranch master
-git config --global advice.skippedCherryPicks false
+echo ".header on
+.mode table" > ~/.sqliterc
 
 # =========================================================================
-# Configure Firefox
+# Disable suspend and hibernation (https://wiki.debian.org/Suspend)
 # =========================================================================
 
-about:config
-toolkit.tabbox.switchByScrolling => true
+sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 
 # =========================================================================
-# Set timezone
+# Configure keyboard layouts
 # =========================================================================
 
-sudo timedatectl set-timezone Europe/Bucharest
+sudo cp /usr/share/X11/xkb/symbols/ch /usr/share/X11/xkb/symbols/ch_backup_$(date +%F_%T)
+
+echo '
+// DE_US / programmer layout.
+// Based on:
+// https://faiwer.ru/content/175-custom_english-german_layout_for_ubuntu
+
+default  partial alphanumeric_keys
+xkb_symbols "basic" {
+    include "us"
+
+    name[Group1]="German";
+
+    key <AC01> { [     a,          A,    adiaeresis,       Adiaeresis ] };
+    key <AC02> { [     s,          S,    ssharp,           section    ] };
+    key <AD07> { [     u,          U,    udiaeresis,       Udiaeresis ] };
+    key <AD09> { [     o,          O,    odiaeresis,       Odiaeresis ] };
+    key <AD03> { [     e,          E,    eacute,           Eacute     ] };
+
+    include "level3(ralt_switch)"
+};' | sudo tee /usr/share/X11/xkb/symbols/ch
+
+xfconf-query -c keyboard-layout -p /Default/XkbLayout -n -t string -s "ro,ch"
 
 # =========================================================================
 # Set first day of week to Monday (https://wiki.debian.org/Locale)
@@ -271,27 +287,40 @@ sudo locale-gen
 sudo cp /etc/default/locale /etc/default/locale_backup_$(date +%F_%T)
 echo '
 LC_TIME="en_GB.UTF-8"
-' | sudo tee -a /etc/default/locale > /dev/null
-
+' | sudo tee -a /etc/default/locale
 
 # =========================================================================
-# Disable suspend and hibernation
-# https://wiki.debian.org/Suspend#Disable_suspend_and_hibernation
+# Set timezone
 # =========================================================================
-sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 
+sudo timedatectl set-timezone Europe/Bucharest
+
+# =========================================================================
+# Configure fonts
+# =========================================================================
+
+# Remove Noto fonts so the system may fall back to DejaVu.
+sudo cp /usr/share/fontconfig/conf.avail/60-latin.conf /usr/share/fontconfig/conf.avail/60-latin.conf_backup_$(date +%F_%T)
+sudo sed -i 's/.*Noto.*//g' /usr/share/fontconfig/conf.avail/60-latin.conf
 
 # =========================================================================
 # XFCE settings
 # =========================================================================
 
-# Power Manager => Show label: Percentage
-xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/show-panel-label -n -t int -s "1"
-
 # Power Manager => System => When laptop lid is closed: => Switch off display
 xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/lid-action-on-ac -n -t int -s "0"
 xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/lid-action-on-battery -n -t int -s "0"
 
+# Power Manager => Show label: Percentage
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/show-panel-label -n -t int -s "1"
+
+# Disable power manager notifications
+xfconf-query -c xfce4-notifyd -p /applications/muted_applications -n -t string -s "xfce4-power-manager" -a
+
+# Tasklist
+tasklist_plugin=$(xfconf-query -c xfce4-panel -p /plugins -l -v | grep tasklist | cut -d' ' -f 1)
+# Panel => Window Buttons => Group windows by application
+xfconf-query -c xfce4-panel -p $tasklist_plugin/grouping -n -t bool -s "false"
 
 # Clock
 clock_plugin=$(xfconf-query -c xfce4-panel -p /plugins -l -v | grep clock | cut -d' ' -f 1)
@@ -300,62 +329,43 @@ xfconf-query -c xfce4-panel -p $clock_plugin/digital-layout -n -t int -s "3"
 # Font:
 xfconf-query -c xfce4-panel -p $clock_plugin/digital-time-font -n -t string -s "Sans 11"
 
-# Tasklist
-tasklist_plugin=$(xfconf-query -c xfce4-panel -p /plugins -l -v | grep tasklist | cut -d' ' -f 1)
-# Panel => Window Buttons => Group windows by application
-xfconf-query -c xfce4-panel -p $tasklist_plugin/grouping -n -t bool -s "false"
-
 # thunar => Preferences => View new folders using: Compact List View
 xfconf-query -c thunar -p /default-view -n -t string -s "ThunarCompactView"
-
 
 # thunar => Close window with multiple tabs? => Do not ask me again
 xfconf-query -c thunar -p /misc-confirm-close-multiple-tabs -n -t bool -s "false"
 
-
 # xfwm4 => mousewheel_rollup
 xfconf-query -c xfwm4 -p /general/mousewheel_rollup -n -t bool -s "false"
-
 
 # Workspace Switcher => Number of workspaces: 2
 xfconf-query -c xfwm4 -p /general/workspace_count -n -t int -s "2"
 
-
-# Disable power manager notifications
-xfconf-query -c xfce4-notifyd -p /applications/muted_applications -n -t string -s "xfce4-power-manager" -a
-
-
 # Launch GNOME services on startup
 xfconf-query -c xfce4-session -p /compat/LaunchGNOME -n -t bool -s "true"
-
 
 # Log Out => Save session for future logins
 xfconf-query -c xfce4-session -p /general/SaveOnExit -n -t bool -s "false"
 
+# Desktop icons
+xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-filesystem -n -t bool -s "false"
 
 # Keyboard
 xfconf-query -c keyboards -p /Default/KeyRepeat/Delay -n -t int -s "250"
 xfconf-query -c keyboards -p /Default/KeyRepeat/Rate -n -t int -s "50"
 xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/Print" -n -t string -s "xfce4-screenshooter -f -s /home/$USER/Desktop/"
 
-
 # Mouse
 xfconf-query -c pointers -p /Logitech_Wireless_Mouse/Acceleration -n -t double -s "8"
-
-
-# Desktop icons
-xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-filesystem -n -t bool -s "false"
 
 # =========================================================================
 # XFCE theme (https://github.com/vinceliuice/Matcha-gtk-theme)
 # =========================================================================
 
-sudo apt-get install gtk2-engines-murrine gtk2-engines-pixbuf
-
-# https://github.com/vinceliuice/Matcha-gtk-theme/releases => Latest release => Source code (tar.gz)
-# tar -xf Matcha-gtk-theme-...
-# cd Matcha-gtk-theme-...
-# ./install.sh
+wget https://github.com/vinceliuice/Matcha-gtk-theme/archive/refs/tags/2023-04-03.tar.gz
+tar -xzvf 2023-04-03.tar.gz
+cd Matcha-gtk-theme-2023-04-03/
+./install.sh
 
 xfconf-query -c xsettings -p /Net/ThemeName -n -t string -s "Matcha-dark-sea"
 
@@ -363,93 +373,44 @@ xfconf-query -c xsettings -p /Net/ThemeName -n -t string -s "Matcha-dark-sea"
 # XFCE icon theme (https://github.com/PapirusDevelopmentTeam/papirus-icon-theme)
 # =========================================================================
 
-sudo apt-get install papirus-icon-theme
+sudo apt install papirus-icon-theme
 
 xfconf-query -c xsettings -p /Net/IconThemeName -n -t string -s "Papirus"
 
 # =========================================================================
-# xfce4-screenshooter
+# Fix ~/.config/mimeapps.list
 # =========================================================================
 
-echo "app=/usr/bin/display-im6.q16
-custom_action_command=none
-last_user=
-last_extension=png
-screenshot_dir=file:/home/$USER/Desktop
-enable_imgur_upload=true
-show_in_folder=false
-action=1
-delay=0
-region=1
-show_mouse=0
-show_border=1" > /home/$USER/.config/xfce4/xfce4-screenshooter
-
-# =========================================================================
-# xarchiver
-# =========================================================================
-
-mkdir -p /home/$USER/.config/xarchiver/
-
-echo "[xarchiver]
-preferred_format=17
-prefer_unzip=true
-confirm_deletion=true
-sort_filename_content=false
-store_output=false
-icon_size=2
-show_archive_comment=false
-show_sidebar=true
-show_location_bar=true
-show_toolbar=true
-preferred_custom_cmd=
-preferred_temp_dir=/tmp
-preferred_extract_dir=/home/$USER/Desktop
-allow_sub_dir=0
-ensure_directory=true
-overwrite=false
-full_path=2
-touch=false
-fresh=false
-update=false
-store_path=false
-updadd=true
-freshen=false
-recurse=true
-solid_archive=false
-remove_files=false" > /home/$USER/.config/xarchiver/xarchiverrc
-
-# =========================================================================
-# Fix /home/$USER/.config/mimeapps.list
-# =========================================================================
-
-echo "[Default Applications]" > /home/$USER/.config/mimeapps.list
-
+echo "[Default Applications]" > ~/.config/mimeapps.list
 
 cat /usr/share/applications/mimeinfo.cache | grep 'vim.desktop' | cut -d'=' -f 1 | while read -r mime;
 do
     echo "$mime=sublime_text.desktop;"
-done >> /home/$USER/.config/mimeapps.list
-
+done >> ~/.config/mimeapps.list
 
 cat /usr/share/applications/mimeinfo.cache | grep -E 'text/html|text/xml' | cut -d'=' -f 1 | while read -r mime;
 do
     echo "$mime=chromium.desktop;"
-done >> /home/$USER/.config/mimeapps.list
-
+done >> ~/.config/mimeapps.list
 
 cat /usr/share/applications/mimeinfo.cache | grep 'display-im6.q16.desktop' | cut -d'=' -f 1 | while read -r mime;
 do
     echo "$mime=viewnior.desktop;"
-done >> /home/$USER/.config/mimeapps.list
-
+done >> ~/.config/mimeapps.list
 
 cat /usr/share/applications/mimeinfo.cache | grep 'image/pdf' | cut -d'=' -f 1 | while read -r mime;
 do
     echo "$mime=atril.desktop;"
-done >> /home/$USER/.config/mimeapps.list
-
+done >> ~/.config/mimeapps.list
 
 cat /usr/share/applications/mimeinfo.cache | grep -E 'QuodLibet|Parole' | cut -d'=' -f 1 | while read -r mime;
 do
     echo "$mime=vlc.desktop;"
-done >> /home/$USER/.config/mimeapps.list
+done >> ~/.config/mimeapps.list
+
+# =========================================================================
+# Configure Firefox
+# =========================================================================
+
+about:config
+toolkit.tabbox.switchByScrolling => true
