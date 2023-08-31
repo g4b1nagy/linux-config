aa7b04a8cc17fe78e17fb598ba2e37246c74555f
Gabi Nagy <gabrian.nagy@gmail.com>
2023-08-31 17:40:36 +0200

    Update debian-12-bookworm-xfce.sh

--- debian-12-bookworm-xfce.sh
+++ debian-12-bookworm-xfce.sh
@@ -46,7 +46,7 @@ packages=(
     mtp-tools
 )
 
-# Add Sublime Text apt repository.
+# Add Sublime Text apt repository (https://www.sublimetext.com/docs/linux_repositories.html)
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
